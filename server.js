9b47f326d57bd9fa06fea2971711f0215768f873
Currentsoftware <michel@current.nl>
2013-06-20 01:50:03 +0200

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   
-  var sql = require('node-sqlserver');
+  //var sql = require('node-sqlserver');
 var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:hmkszo8clf.database.windows.net,1433;Database=appFramAkRXFwnG7;Uid=appFramework@hmkszo8clf;Pwd=6858ABcd;Encrypt=yes;Connection Timeout=30;";
 
 res.end("hello world");
