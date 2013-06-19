d63f59b81a526feb0103f51dbbda3d20bf3905fc
Currentsoftware <michel@current.nl>
2013-06-20 01:46:49 +0200

    a

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ http.createServer(function(req, res) {
   var sql = require('node-sqlserver');
 var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:hmkszo8clf.database.windows.net,1433;Database=appFramAkRXFwnG7;Uid=appFramework@hmkszo8clf;Pwd=6858ABcd;Encrypt=yes;Connection Timeout=30;";
 
-var stmt = sql.query(conn_str, "SELECT * FROM Languages");
+//var stmt = sql.query(conn_str, "SELECT * FROM Languages");
 
-stmt.on('meta', function (meta) { res.write("We've received the metadata"); });
-stmt.on('row', function (idx) { res.write("We've started receiving a row"); });
-stmt.on('column', function (idx, data, more) { res.write(idx + ":" + data);});
-stmt.on('done', function () { res.end("All done!"); });
-stmt.on('error', function (err) { res.write("We had an error: " + err); });
+//stmt.on('meta', function (meta) { res.write("We've received the metadata"); });
+//stmt.on('row', function (idx) { res.write("We've started receiving a row"); });
+//stmt.on('column', function (idx, data, more) { res.write(idx + ":" + data);});
+//stmt.on('done', function () { res.end("All done!"); });
+//stmt.on('error', function (err) { res.write("We had an error: " + err); });
 
 }).listen(port);
