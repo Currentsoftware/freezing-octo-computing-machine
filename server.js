495dd5b072ffe64430142f5e8f5597dd286fb996
Currentsoftware <michel@current.nl>
2013-06-20 01:47:58 +0200

    a

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ http.createServer(function(req, res) {
   var sql = require('node-sqlserver');
 var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:hmkszo8clf.database.windows.net,1433;Database=appFramAkRXFwnG7;Uid=appFramework@hmkszo8clf;Pwd=6858ABcd;Encrypt=yes;Connection Timeout=30;";
 
+res.end("hello world");
 //var stmt = sql.query(conn_str, "SELECT * FROM Languages");
 
 //stmt.on('meta', function (meta) { res.write("We've received the metadata"); });
